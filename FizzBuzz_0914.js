1c413fab6a584e64d2a92fb82469864ffa4e9748
sunny-pochi <67634560+sunny-pochi@users.noreply.github.com>
2020-09-20 07:48:08 +0900

    JavaScript課題②に取り組みました //修正
    修正箇所
    ・文字列は""ではなく''で囲む
    ・不要なconsoleやコメントは全て削除
    ・ここと28行目の条件を含めた条件を一行で
    
    お手数をおかけいたしますが、ご確認お願いいたします

--- FizzBuzz_0914.js
+++ FizzBuzz_0914.js
@@ -4,18 +4,10 @@ const fizzNum = document.getElementById("fizz-num");
 const buzzNum = document.getElementById("buzz-num");
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", calFizzBuzz, false);
-// const newFizz = parseInt(fizzNum.value);
-// const newBuzz = parseInt(buzzNum.value);
 
 function calFizzBuzz() {
   console.log(fizzNum.value);
   console.log(buzzNum.value);
-  // console.log(newFizz);
-  // console.log(newBuzz);
-  console.log(Number.isInteger(parseInt(fizzNum.value)));
-  console.log(Number.isInteger(parseInt(buzzNum.value)));
-
-  console.log(!fizzNum.value.match(/[^0-9]+/));
 
   if (!fizzNum.value || !buzzNum.value) {
     alert("文字を入力してください");
@@ -29,18 +21,10 @@ function calFizzBuzz() {
     alert("数字を入力してください");
     return;
   }
-
-  // } else if (!fizzNum.value.match(/[^0-9]+/)) {
-  //   alert("整数値を入力してください");
-  // } else if (!buzzNum.value.match(/[^0-9]+/)) {
-  //   alert("整数値を入力してください");
-  // }
-  
-  if (!Number.isInteger(Number(fizzNum.value))) {
-    alert("整数値を入力してください");
-    return;
-  }
-  if (!Number.isInteger(Number(buzzNum.value))) {
+  if (
+    !Number.isInteger(Number(fizzNum.value)) ||
+    !Number.isInteger(Number(buzzNum.value))
+  ) {
     alert("整数値を入力してください");
     return;
   }
@@ -61,7 +45,6 @@ function calFizzBuzz() {
     }
   }
 
-  // 入力部分を初期化で消去
   fizzNum.value = "";
   buzzNum.value = "";
 }
